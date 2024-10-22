3df92b7f9c3d948758709e8d899d8e03ba0afd0e
Dxnxex <dxnxex@seznam.cz>
2024-10-22 22:43:26 +0200

    Požadovaná stylizace, logo, footer

--- scripts/Scroll.js
+++ scripts/Scroll.js
@@ -1,15 +1,25 @@
+//LOGO
+window.addEventListener('scroll', function() {
+let scrollTop = window.scrollY || document.documentElement.scrollTop;
+let viewportHeight = window.innerHeight / 5;
+let background = document.querySelector('.backgroundLogo');
 
-    window.addEventListener('scroll', function() {
-        let scrollTop = window.scrollY || document.documentElement.scrollTop;
-        let viewportHeight = window.innerHeight/5;
-        let background = document.querySelector('.backgroundLogo');
-        if (scrollTop > viewportHeight) {background.style.opacity = '0';  } else {background.style.opacity = '1'; }
-    });
+//Nekliknutí v případě, že je schovaný
+if (scrollTop > viewportHeight) {
+    background.style.opacity = '0'; 
+    background.style.pointerEvents = 'none'; // Zakázat kliknutí
+} else {
+    background.style.opacity = '1'; 
+    background.style.pointerEvents = 'auto'; // Povolit kliknutí
+}
+});
 
 
-    window.addEventListener('scroll', function() {
-        let scrollTop = window.scrollY || document.documentElement.scrollTop;
-        let viewportHeight = window.innerHeight/100;
-        let navigace = document.querySelector('.navbar');
-        if (scrollTop > viewportHeight) {navigace.style.opacity = '0';  } else {navigace.style.opacity = '1'; }
-    });
+
+
+window.addEventListener('scroll', function() {
+    let scrollTop = window.scrollY || document.documentElement.scrollTop;
+    let viewportHeight = window.innerHeight/100;
+    let navigace = document.querySelector('.navbar');
+    if (scrollTop > viewportHeight) {navigace.style.opacity = '0';  } else {navigace.style.opacity = '1'; }
+});
